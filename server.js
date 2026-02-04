bd5b70410b2bfeb0c6c14fd3fddb13cd1d7deb8f
Damitha Nayanajith <ddnayanajith871@gmail.com>
2026-02-04 22:56:23 +0530

    Move site to docs for GitHub Pages

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "docs")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "docs", "index.html"));
 });
 
 app.listen(PORT, () => {
